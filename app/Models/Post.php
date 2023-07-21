4e0f2569fd9f145b6cce48286dcea6cf1c9dc4c1
Cody Alan Stubbs <castubbs3@gmail.com>
2023-07-21 10:21:15 -0400

    Create eloquent relationship btwn posts and categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Post extends Model
 {
     use HasFactory;
     
-    protected $gaurded = [];
+    protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
