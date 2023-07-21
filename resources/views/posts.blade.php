754f4012040ffe9c45b0e1a08c6a8dcd9f95193f
Cody Alan Stubbs <castubbs3@gmail.com>
2023-07-21 09:40:34 -0400

    Start build migrations and use route model binding

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,9 @@
 <x-layout html>
     @foreach ($posts as $post)
-        <article class="{{  $loop->even ? 'mb-6' : '' }}">
+        <article>
             <h1>
-                <a href="./posts/<?= $post->slug; ?>">
-                    {{ $post->title }}
+                <a href="./posts/{{ $post->slug }}">
+                    {!! $post->title !!}
                 </a>
             </h1>
             <div>
