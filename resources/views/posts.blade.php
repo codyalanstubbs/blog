92592531d896cd9ce81409534b46375be78b056c
Cody Alan Stubbs <castubbs3@gmail.com>
2023-07-21 08:27:55 -0400

    Complete sections 1 to 3 of Laravel 8 from Scratch

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<x-layout html>
+    @foreach ($posts as $post)
+        <article class="{{  $loop->even ? 'mb-6' : '' }}">
+            <h1>
+                <a href="./posts/<?= $post->slug; ?>">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
